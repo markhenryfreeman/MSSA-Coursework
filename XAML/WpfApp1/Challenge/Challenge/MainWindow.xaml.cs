febec5abec8ae1cbb6702e9eef50f908272374f5
Mark Freeman <83133380+markhenryfreeman@users.noreply.github.com>
2021-11-28 12:28:17 -0600

    Add files via upload

---
+++ XAML/WpfApp1/Challenge/Challenge/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Challenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void addSize(object a, EventArgs e)
+        {
+
+            Debug.WriteLineIf(true, "Radio Buttons Pushed");
+        }
+
+       public void addCondiments (object a, EventArgs e)
+        {
+            Debug.WriteLineIf(true,"Checkbox Buttons Pushed");
+
+        }
+
+        public void typeChosen (object a, EventArgs e)
+        {
+            Debug.WriteLine("Combo Box Pushed");
+        }
+       
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
